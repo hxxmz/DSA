776f7a348f2f53f7eaf4beb41b5f7911ff8667e4
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-16 10:49:22 +0500

    Create 75.py
    learnt dnf, first sol ez asf 1 min sol thought

---
+++ languages/py/problems/leetcode/Arrays & Strings/75.py
@@ -0,0 +1,45 @@
+def sortColors(nums) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    colors = {}
+    for color in nums:
+        if color not in colors:
+            colors[color] = 1
+            continue
+        colors[color] += 1
+    
+    current = 0
+    for i in range(len(nums)):
+        if colors[current] == 0:
+            current += 1
+        nums[i] = current
+        colors[current] -= 1
+
+    print(nums)
+
+def sortColorsDNF(nums) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    left, right = 0, len(nums)-1
+    current = 0
+    
+    while current <= right:
+        if nums[current] == 0:
+            nums[left], nums[current] = nums[current], nums[left]
+            left +=1
+            current += 1
+        elif nums[current] == 1:
+            current += 1
+        elif nums[current] == 2:
+            nums[current], nums[right] = nums[right], nums[current]
+            right -= 1
+
+    print(nums)
+
+sortColors(nums = [2,0,2,1,1,0])
+sortColors(nums = [2,0,1])
+
+sortColorsDNF(nums = [2,0,2,1,1,0])
+sortColorsDNF(nums = [2,0,1])
