c281913b380a82d2d1a6abbadd7c17d8e8a5d9c8
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-17 20:18:44 +0500

    Update 238.py
    completed

--- problems/leetcode/Arrays & Strings/238.py
+++ problems/leetcode/Arrays & Strings/238.py
@@ -1,21 +1,27 @@
 def productExceptSelf(nums):
     length = len(nums)
-    answer = [0]*length
-    
-    prefixProduct = [0] * length
+    answer = [0] * length
+
+    # calculate the accumulative product from the left and right of the each index  
+    prefix_products = [0] * length
     product = 1
     for i in range(length):
-        prefixProduct[i] = product
+        prefix_products[i] = product
         product *= nums[i]
-    
+    # print(prefix_products)
+
     product = 1
+    suffix_products = [0] * length
+    for i in range(length-1, -1, -1):
+        suffix_products[i] = product
+        product *= nums[i]
+    # print(suffix_products)
 
-    print(prefixProduct)
-
-
-
+    # product of current index is the product of sub arrays left and right to it
+    for i in range(length):
+        answer[i] = prefix_products[i] * suffix_products[i]
 
     return answer
 
 print(productExceptSelf(nums = [1,2,3,4]))
-# print(productExceptSelf(nums = [-1,1,0,-3,3]))
+print(productExceptSelf(nums = [-1,1,0,-3,3]))
