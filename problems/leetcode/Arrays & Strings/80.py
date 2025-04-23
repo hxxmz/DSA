6437cc895936f30cc5ad417d6c9e43f35ef76fa9
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-23 14:12:04 +0500

    Update 80.py
    solved :)

--- problems/leetcode/Arrays & Strings/80.py
+++ problems/leetcode/Arrays & Strings/80.py
@@ -1,6 +1,11 @@
 def removeDuplicates(nums) -> int:
-    pass
-    return None
+    l = 2
+    for r in range(2, len(nums)):
+        if nums[r] != nums[l-2]:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+    print(nums)
+    return l
 
 print(removeDuplicates([1,1,1,2,2,3]))
 print(removeDuplicates([0,0,1,1,1,1,2,3,3]))
