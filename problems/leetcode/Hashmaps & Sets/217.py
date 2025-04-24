f88983e874b532c86f81f45780cdb3350497713c
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-24 14:43:48 +0500

    Create 217.py
    solved

---
+++ problems/leetcode/Hashmaps & Sets/217.py
@@ -0,0 +1,8 @@
+# 217. Contains Duplicate
+
+def containsDuplicate(nums) -> bool:
+    return len(set(nums)) != len(nums)
+
+print(containsDuplicate(nums = [1,2,3,1]))
+print(containsDuplicate(nums = [1,2,3,4]))
+print(containsDuplicate(nums = [1,1,1,3,3,4,3,2,4,2]))
