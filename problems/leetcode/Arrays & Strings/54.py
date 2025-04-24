b44b09d809a90da504b8b35472edcdca3ca64f72
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-24 10:41:46 +0500

    Update 54.py
    incomplete, will do it later

--- problems/leetcode/Arrays & Strings/54.py
+++ problems/leetcode/Arrays & Strings/54.py
@@ -1,10 +1,22 @@
 def spiralOrder(matrix):
     m = len(matrix)
     n = len(matrix[0])
-    answer = [0] * (m*n)
+    answer = [0] * (m * n)
 
     lb = 0
     rb = 0
+
+    direction = 1
+    horizontal = True
+
+    # to handle bounds further maybe place an index on answer array to know how many elements scanned 
+    # and cut off main loop through that, because middle indices require more than one appearances
+    # think of something to handle bounds
+    # you can also use directions template to escape many conditions;
+    # use dirs (r,c) = ( (0,1),(1,0),(0,-1),(-1,0) ) , 0 for constant / non-zero for progression
+
+    # to be continued
+   
 
     print(len(answer))
 
