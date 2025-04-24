56b188f37d5f602d27e860ad3e430bd5db44a916
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-24 15:38:32 +0500

    Update 383.py
    optimized

--- problems/leetcode/Hashmaps & Sets/383.py
+++ problems/leetcode/Hashmaps & Sets/383.py
@@ -24,7 +24,33 @@ def canConstruct(ransomNote: str, magazine: str) -> bool:
             return False
     return True
 
+def canConstruct_opt(ransomNote: str, magazine: str) -> bool:
+    """
+    What I can also do is, ransomNote_count happens as it is.
+    We then loop over magazine and decrement everytime a letter appears in ransom.
+    After the loop, I check if sum of all values <=0, if so, then letters in magazine fulfill ransom letter requirements,
+    otherwise (sum>0) then all of the letters of the ransom note were not accounted for.
+    """
+    ransomNote_count = {}
+    for letter in ransomNote:
+        ransomNote_count[letter] = ransomNote_count.get(letter, 0) + 1
+        
+    for letter in magazine:
+        if letter in ransomNote_count:
+            ransomNote_count[letter] -= 1
+    
+    count = 0
+    for val in ransomNote_count.values():
+        count += val
+    
+    return count <= 0
+
 print(canConstruct(ransomNote = "a", magazine = "b"))
 print(canConstruct(ransomNote = "aa", magazine = "ab"))
 print(canConstruct(ransomNote = "aa", magazine = "aab"))
 print(canConstruct(ransomNote = "aab", magazine = "aaabbb"))
+
+print(canConstruct_opt(ransomNote = "a", magazine = "b"))
+print(canConstruct_opt(ransomNote = "aa", magazine = "ab"))
+print(canConstruct_opt(ransomNote = "aa", magazine = "aab"))
+print(canConstruct_opt(ransomNote = "aab", magazine = "aaabbb"))
