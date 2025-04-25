e128a31c340d4681ca3d7fcf3fd6c385b1c15183
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-25 15:11:03 +0500

    Update 49.py
    solved

--- problems/leetcode/Hashmaps & Sets/49.py
+++ problems/leetcode/Hashmaps & Sets/49.py
@@ -3,20 +3,21 @@ from typing import List
 
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
 
-    hashmap = {}
-
-    for word in strs:
-        
-        set_weight = 0
-
-        print(word)
-
     # initially thought to use word weight as signatures and set weight to avoid collision on weights
     # better solution can also be char count tuple, instead of assigning weights, i overwite count on index
     # use those tuples as keys to group them together in a hashmap
 
-    pass
+    hashmap = {}
+    for word in strs:
+        char_counts = [0] * 26
+        for letter in word:
+            idx = ord(letter) - ord("a")
+            char_counts[idx] += 1
+        char_key = tuple(char_counts)
+        hashmap.setdefault(char_key, []).append(word)
 
-groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]) # [["bat"],["nat","tan"],["ate","eat","tea"]]
-groupAnagrams(strs = [""]) # [[""]]
-groupAnagrams(strs = ["a"]) # [["a"]]
+    return [val for val in hashmap.values()]
+
+print(groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"])) # [["bat"],["nat","tan"],["ate","eat","tea"]]
+print(groupAnagrams(strs = [""])) # [[""]]
+print(groupAnagrams(strs = ["a"])) # [["a"]]
