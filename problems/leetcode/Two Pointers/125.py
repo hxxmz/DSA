f1da4a60481c62d4cfe6a59a7f716cd1accc9e77
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-26 12:51:51 +0500

    Create 125.py
    solved

---
+++ problems/leetcode/Two Pointers/125.py
@@ -0,0 +1,22 @@
+# 125. Valid Palindrome
+import re
+
+def isPalindrome(s: str) -> bool:
+    
+    s = re.sub(r'[^a-zA-Z]', '', s).lower()
+
+    if len(s) == 0:
+       return True
+
+    l, r = 0, len(s) - 1
+    while l < r:
+        if s[l] != s[r]:
+           return False
+        l += 1
+        r -= 1
+    
+    return True
+
+print(isPalindrome(s = "A man, a plan, a canal: Panama"))
+print(isPalindrome(s = "race a car"))
+print(isPalindrome(s = " "))
