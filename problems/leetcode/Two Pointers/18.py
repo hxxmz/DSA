d156b514e7d64d484923e8aa111e874389fbf113
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-28 13:11:42 +0500

    Create 18.py
    incomplete

---
+++ problems/leetcode/Two Pointers/18.py
@@ -0,0 +1,8 @@
+# 18. 4Sum
+from typing import List
+
+def fourSum(nums: List[int], target: int) -> List[List[int]]:
+    pass
+
+print(fourSum(nums = [1,0,-1,0,-2,2], target = 0))
+print(fourSum(nums = [2,2,2,2,2], target = 8))
