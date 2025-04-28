abecea2887621573cfb29e93a1432b07eb85be55
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-04-28 11:26:55 +0500

    Update 15.py
    optimized to skip same elements

--- problems/leetcode/Two Pointers/15.py
+++ problems/leetcode/Two Pointers/15.py
@@ -2,25 +2,33 @@
 from typing import List
 
 def threeSum(nums: List[int]) -> List[List[int]]:
-    # using built-in sort, not the main concern
     nums.sort()
     answer = []
     length = len(nums)
-    # three indices i, j, k && a: nums[i], b: nums[j], c: nums[k]
+
     for i in range(length):
+
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
         j = i + 1
         k = length - 1
-        while j < k:           
-            # check if they sum to 0
+
+        while j < k:
             if nums[i] + nums[j] + nums[k] > 0:
                 k -= 1
             elif nums[i] + nums[j] + nums[k] < 0:
                 j += 1
             else:
-                # print(f"[{i}] a: {nums[i]} | [{j}] b: {nums[j]} | [{k}] c: {nums[k]}")
                 answer.append([nums[i], nums[j], nums[k]])
                 j += 1
                 k -= 1
+
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+                while j < k and nums[k] == nums[k + 1]:
+                    k -= 1
+
     return answer
 
 print(threeSum(nums = [-1,0,1,2,-1,-4]))
