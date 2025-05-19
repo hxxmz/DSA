ce906b7771213b4142ba781fc2722b9b1e7fccd6
Hamza <82061179+hxxmz@users.noreply.github.com>
2025-05-19 14:42:25 +0500

    Update 643.py
    removed two pointers as it was unnecessary, renamed var

--- problems/leetcode/Sliding Window/643.py
+++ problems/leetcode/Sliding Window/643.py
@@ -2,6 +2,19 @@ from typing import List
 # 643. Maximum Average Subarray I
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr_sum = 0
+        for i in range(k):
+            curr_sum += nums[i]
+        
+        max_sum = curr_sum
+        for i in range(k, len(nums)):
+            curr_sum += nums[i]
+            curr_sum -= nums[i - k]
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum / k
+    
+    def findMaxAverage_TwoPtrs(self, nums: List[int], k: int) -> float:
         summ = 0
         for i in range(k):
             summ += nums[i]
